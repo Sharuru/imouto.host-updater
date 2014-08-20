71d9e6d56537c328807d9e859d01fb2f01b5aee6
Mave <i@0w0.re>
2014-08-20 17:36:41 +0800

    need fix Connection Error to projecth.us

--- updater.py
+++ updater.py
@@ -1,31 +1,25 @@
 #-*-coding:utf-8 -*-
 __author__ = 'Mave'
 
-import requests
+import re
+import urllib2
 
 reload(__import__('sys')).setdefaultencoding('utf-8')
 
-
 def linker(url):
     linker_try_times = 3
-    headers = {
-    'User-Agent': 'My User Agent 1.0',
-    'From': 'youremail@domain.com'  # This is another valid field
-    }
-    response = requests.get(url, headers=headers)
-    print response.status_code
-    print response.text
+    request = urllib2.Request(url)
+    request.add_header('User-Agent', 'Macho Macho Man')
     for times in range(linker_try_times):
         print 'Connecting...',
         try:
             print 'tried'
-            response = urllib2.urlopen(request, timeout=5)
-            print response.read()
-            #return response.read()
+            response = urllib2.urlopen(url, timeout=5)
+            return response.read()
         except:
             print 'Failed, Try Again.'
             pass
 
-urls = 'https://www.projecth.us/sources'
+urls = 'https://www.projecth.us/sources/'
 #urls = 'https://www.alipay.com'
-linker(urls)
+print linker(urls)
