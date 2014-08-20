2fad0542ae6ff5364cd248f728f1460a12e16fc0
Mave <i@0w0.re>
2014-08-20 19:39:42 +0800

    check remote version

--- updater.py
+++ updater.py
@@ -3,8 +3,12 @@ __author__ = 'Mave'
 
 import re
 import urllib2
+import platform
 
 reload(__import__('sys')).setdefaultencoding('utf-8')
+
+system_type = platform.system()
+
 
 def linker(url):
     linker_try_times = 3
@@ -13,13 +17,54 @@ def linker(url):
     for times in range(linker_try_times):
         print 'Connecting...',
         try:
-            print 'tried'
             response = urllib2.urlopen(url, timeout=5)
+            print 'Success.'
             return response.read()
         except:
             print 'Failed, Try Again.'
             pass
+        if times == 2:
+            print 'Connection Failed.'
+            print 'Please check your hosts or try again later.'
+            exit()
+
+
+def get_hosts(source):
+    if source == 9:      # imouto.hosts' id is 9
+        hosts = re.compile('<a href="/sources/9/(.*)/hosts.*</a>')
+        return hosts.findall(content)[0]
+
+
+def check_hosts_version(hosts_data):
+    version = re.compile('UPDATE_TIME (.*)')
+    return version.findall(hosts_data)[0]
+
+
+def check_remote_version(source):
+    if source == 9:     # imouto.hosts' id is 9
+        version = re.compile('<img src="/sources/9/icon.jpg".*?>.*?</th>.*?<th>.*?</th>.*?<th>(.*?)</th>', re.S)
+        return version.findall(content)[0]
+
 
 urls = 'https://www.projecth.us/sources/'
-#urls = 'https://www.alipay.com'
-print linker(urls)
+
+content = linker(urls).decode('utf-8')
+
+source_id = 9       # imouto.hosts' id is 9
+print 'Finding imouto.hosts...'
+
+print 'Latest update time is: ' + check_remote_version(9)
+
+
+#download_url = urls + str(source_id) + '/' + get_hosts(source_id) + '/hosts'
+#print 'Downloading latest imouto.hosts from ' + download_url,
+
+#remote_hosts = urllib2.urlopen(download_url)
+#remote_hosts_data = remote_hosts.read()
+#print 'Success.'
+
+
+
+
+
+
